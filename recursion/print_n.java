2c1c12f87d98538beac461e3894756ffb3e8de30
imvik2000 <imvik2000@gmail.com>
2022-01-31 19:19:08 +0530

    first commit

---
+++ recursion/print_n.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class print_n
+{
+ public static void print_num(int n)
+ {
+     if(n==0)
+     {
+         return;
+     }
+    //  System.out.println(n);
+     print_num(n-1);
+     System.out.println(n);
+ }
+
+
+    public static void main(String args[])
+{
+    Scanner sc= new Scanner(System.in);
+    int n;
+    n=sc.nextInt();
+    
+    print_num(n);
+sc.close();
+}
+}
